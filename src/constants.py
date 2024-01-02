30a2b54b17d37fbc2271b39755e6e4d8e2bb096c
Serhii5465 <serhiiartiukh5@gmail.com>
2024-01-02 18:43:44 +0200

    init

---
+++ src/constants.py
@@ -0,0 +1,24 @@
+from py_exec_cmd import exec_cmd
+
+def NAME_SYNC_DIR() -> str:
+    return 'Music/'
+
+# # Test
+# def ROOT_UNIX_SRC_DIR() -> str:
+#     return '/cygdrive/f/media/'
+
+def ROOT_UNIX_SRC_DIR() -> str:
+    return '/cygdrive/e/media/'
+
+def FULL_PATH_UNIX_SRC_ROOT() -> str:
+    return ROOT_UNIX_SRC_DIR() + NAME_SYNC_DIR()
+
+def WIN_SRC_DIR() -> str:
+    return exec_cmd.get_bety_cmd_out(['cygpath.exe', '--windows', FULL_PATH_UNIX_SRC_ROOT()])
+
+# # Test
+# def ROOT_DEST_DIR() -> str:
+#     return '/storage/self/primary/'
+
+def ROOT_DEST_DIR() -> str:
+    return '/storage/F32E-95B4/'
